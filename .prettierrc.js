184ba1a8d0088e6587f5247783499a0a42c9455c
steciuk <a.t.steciuk@gmail.com>
2025-06-26 11:57:55 +0200

    chore: Format project

--- .prettierrc.js
+++ .prettierrc.js
@@ -4,33 +4,33 @@
  */
 const config = {
   // Normalize with Biome's defaults
-  "printWidth": 80,
-  "endOfLine": "lf",
-  "arrowParens": "always",
-  "bracketSameLine": false,
-  "bracketSpacing": true,
-  "jsxSingleQuote": false,
-  "quoteProps": "as-needed",
-  "semi": true,
-  "trailingComma": "all",
+  printWidth: 80,
+  endOfLine: "lf",
+  arrowParens: "always",
+  bracketSameLine: false,
+  bracketSpacing: true,
+  jsxSingleQuote: false,
+  quoteProps: "as-needed",
+  semi: true,
+  trailingComma: "all",
   // Adhere to Pragma's formatting style
-  "useTabs": false,
-  "tabWidth": 2,
-  "singleQuote": false,
+  useTabs: false,
+  tabWidth: 2,
+  singleQuote: false,
   // Overrides
-  "overrides": [
+  overrides: [
     {
-      "files": "*.svelte",
-      "options": {
-        "parser": "svelte",
-        "svelteAllowShorthand": true,
-        "svelteIndentScriptAndStyle": true,
-        "svelteSortOrder": "options-scripts-markup-styles",
-        "svelteStrictMode": true,
+      files: "*.svelte",
+      options: {
+        parser: "svelte",
+        svelteAllowShorthand: true,
+        svelteIndentScriptAndStyle: true,
+        svelteSortOrder: "options-scripts-markup-styles",
+        svelteStrictMode: true,
       },
-    }
+    },
   ],
-  "plugins": ["prettier-plugin-svelte"],
-}
+  plugins: ["prettier-plugin-svelte"],
+};
 
 export default config;
--- eslint.config.js
+++ eslint.config.js
@@ -1,51 +1,51 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from 'eslint-plugin-storybook';
+import storybook from "eslint-plugin-storybook";
 
-import prettier from 'eslint-config-prettier';
-import js from '@eslint/js';
-import svelte from 'eslint-plugin-svelte';
-import globals from 'globals';
-import ts from 'typescript-eslint';
-import svelteConfig from './svelte.config.js';
+import prettier from "eslint-config-prettier";
+import js from "@eslint/js";
+import svelte from "eslint-plugin-svelte";
+import globals from "globals";
+import ts from "typescript-eslint";
+import svelteConfig from "./svelte.config.js";
 
 export default ts.config(
-	js.configs.recommended,
-	...ts.configs.recommended,
-	...svelte.configs.recommended,
-	prettier,
-	...svelte.configs.prettier,
-	{
-		languageOptions: {
-			globals: { ...globals.browser, ...globals.node }
-		},
-		rules: {
-			// typescript-eslint strongly recommend that you do not use the no-undef lint rule on TypeScript projects.
-			// see: https://typescript-eslint.io/troubleshooting/faqs/eslint/#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
-			'no-undef': 'off'
-		}
-	},
+  js.configs.recommended,
+  ...ts.configs.recommended,
+  ...svelte.configs.recommended,
+  prettier,
+  ...svelte.configs.prettier,
+  {
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node },
+    },
+    rules: {
+      // typescript-eslint strongly recommend that you do not use the no-undef lint rule on TypeScript projects.
+      // see: https://typescript-eslint.io/troubleshooting/faqs/eslint/#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
+      "no-undef": "off",
+    },
+  },
   {
     // Pragma opts-out from vcs integration and requires explicit ignore declarations
     ignores: [
-      'bun.lock',
-      'bun.lockb',
-      'node_modules/**',
-      'build/**',
-      '.svelte-kit/**',
-      '*storybook.log',
-      'storybook-static/**',
-    ]
+      "bun.lock",
+      "bun.lockb",
+      "node_modules/**",
+      "build/**",
+      ".svelte-kit/**",
+      "*storybook.log",
+      "storybook-static/**",
+    ],
   },
-	{
-		files: ['**/*.svelte', '**/*.svelte.ts', '**/*.svelte.js'],
-		languageOptions: {
-			parserOptions: {
-				projectService: true,
-				extraFileExtensions: ['.svelte'],
-				parser: ts.parser,
-				svelteConfig
-			}
-		}
-	},
-	storybook.configs['flat/recommended']
+  {
+    files: ["**/*.svelte", "**/*.svelte.ts", "**/*.svelte.js"],
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        extraFileExtensions: [".svelte"],
+        parser: ts.parser,
+        svelteConfig,
+      },
+    },
+  },
+  storybook.configs["flat/recommended"],
 );
