0374cc54e34831c2c585c8ef56ea42db5c957ddb
Goulin Khoge <36013798+goulinkh@users.noreply.github.com>
2025-09-19 11:13:23 +0200

    fix(ViteConfig): update the config to work with latest vite version (7.1.6)

--- vite.config.ts
+++ vite.config.ts
@@ -4,45 +4,4 @@ import devtoolsJson from "vite-plugin-devtools-json";
 
 export default defineConfig({
   plugins: [sveltekit(), devtoolsJson()],
-  test: {
-    silent: false,
-    projects: [
-      {
-        extends: "./vite.config.ts",
-        test: {
-          name: "client",
-          environment: "browser",
-          browser: {
-            enabled: true,
-            headless: true,
-            provider: "playwright",
-            instances: [{ browser: "chromium" }],
-          },
-          include: ["src/**/*.svelte.test.{js,ts}"],
-          exclude: ["src/lib/server/**"],
-          setupFiles: ["./vitest-setup-client.ts"],
-        },
-      },
-      {
-        extends: "./vite.config.ts",
-        test: {
-          name: "ssr",
-          environment: "node",
-          include: ["src/**/*.ssr.test.{js,ts}"],
-        },
-      },
-      {
-        extends: "./vite.config.ts",
-        test: {
-          name: "server",
-          environment: "node",
-          include: ["src/**/*.test.{js,ts}"],
-          exclude: [
-            "src/**/*.svelte.test.{js,ts}",
-            "src/**/*.ssr.test.{js,ts}",
-          ],
-        },
-      },
-    ],
-  },
 });
