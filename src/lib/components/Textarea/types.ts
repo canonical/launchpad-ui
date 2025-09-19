ad05f63e26c388de7433a68ea32303b537e2f636
Goulin Khoge <36013798+goulinkh@users.noreply.github.com>
2025-09-19 14:38:57 +0200

    refactor(Textarea): refine props interface to exclude children

--- src/lib/components/Textarea/types.ts
+++ src/lib/components/Textarea/types.ts
@@ -1,8 +1,9 @@
 /* @canonical/generator-ds 0.10.0-experimental.3 */
 
 import type { HTMLTextareaAttributes } from "svelte/elements";
+import type { WithoutChildren } from "$lib/type-utils";
 
-export interface TextareaProps extends HTMLTextareaAttributes {
+export interface TextareaProps extends WithoutChildren<HTMLTextareaAttributes> {
   /**
    * The value of the textarea.
    *
