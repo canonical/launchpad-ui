5998a72725abfad237fee86e8452d3c06c458012
steciuk <a.t.steciuk@gmail.com>
2025-10-07 13:31:43 +0200

    feat: Use SideNavigation in main layout

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -4,12 +4,10 @@ import type { LayoutServerLoad } from "./$types";
 export const load: LayoutServerLoad = async ({ cookies }) => {
   const themeCookie = cookies.get("theme");
   const theme = isTheme(themeCookie) ? themeCookie : "system";
-
-  cookies.set("theme", theme, {
-    path: "/",
-  });
+  const sideNavigation = cookies.get("side-navigation-expanded") !== "false";
 
   return {
     theme,
+    sideNavigation,
   };
 };
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,6 +1,8 @@
 import { isTheme } from "$lib/theme.js";
 import type { Actions } from "./$types";
 
+const aYear = 1000 * 60 * 60 * 24 * 365;
+
 export const actions: Actions = {
   changeTheme: async ({ cookies, request }) => {
     const formData = await request.formData();
@@ -10,6 +12,19 @@ export const actions: Actions = {
 
     cookies.set("theme", theme, {
       path: "/",
+      expires: new Date(Date.now() + aYear),
+    });
+  },
+  toggleSideNavigation: async ({ cookies, request }) => {
+    const formData = await request.formData();
+    const expanded = formData.get("expanded");
+
+    if (expanded !== "true" && expanded !== "false") return;
+
+    cookies.set("side-navigation-expanded", expanded, {
+      path: "/",
+      expires: new Date(Date.now() + aYear),
+      httpOnly: false,
     });
   },
 };
