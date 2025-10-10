51cb49138bfe9610ce29305acaa5eed98d7a9f73
Goulin Khoge <36013798+goulinkh@users.noreply.github.com>
2025-10-10 13:35:12 +0200

    fix(package): of markdown-actions

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,16 @@
+<script>
+  import { MarkdownEditor } from "$lib/components/MarkdownEditor";
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>
   Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
   documentation
 </p>
+
+<MarkdownEditor>
+  <MarkdownEditor.Header>
+    <MarkdownEditor.Toolbar />
+  </MarkdownEditor.Header>
+  <MarkdownEditor.Textarea rows={15} />
+</MarkdownEditor>
