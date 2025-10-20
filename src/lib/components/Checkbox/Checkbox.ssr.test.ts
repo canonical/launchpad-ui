58c1686efebfbce35edcd4c78b4eb671f706a48b
steciuk <a.t.steciuk@gmail.com>
2025-10-20 10:34:59 +0200

    test(Checkbox): Refactor tests

--- src/lib/components/Checkbox/Checkbox.ssr.test.ts
+++ src/lib/components/Checkbox/Checkbox.ssr.test.ts
@@ -1,76 +1,90 @@
-/* @canonical/generator-ds 0.10.0-experimental.0 */
+/* @canonical/generator-ds 0.10.0-experimental.5 */
 
-import { render } from "svelte/server";
+import { render } from "@canonical/svelte-ssr-test";
+import type { RenderResult } from "@canonical/svelte-ssr-test";
 import { describe, expect, it } from "vitest";
 import Component from "./Checkbox.svelte";
 import type { CheckboxProps } from "./types.js";
 
 describe("Checkbox SSR", () => {
-  it("doesn't throw", () => {
-    expect(() => {
-      renderCheckbox();
-    }).not.toThrow();
-  });
-
-  it("renders", () => {
-    const { body } = render(Component);
-    expect(body).toContain('<input type="checkbox"');
-  });
-
-  describe("Basic attributes", () => {
-    it("applies id", () => {
-      const { body } = renderCheckbox({ id: "test-id" });
-      expect(body).toContain('id="test-id"');
+  describe("basics", () => {
+    it("doesn't throw", () => {
+      expect(() => {
+        renderCheckbox({});
+      }).not.toThrow();
     });
 
-    it("applies class", () => {
-      const { body } = renderCheckbox({ class: "test-class" });
-      expect(body).toMatch(/class="[^"]*test-class[^"]*"/);
+    it("renders", () => {
+      const page = renderCheckbox({});
+      expect(componentLocator(page)).toBeInstanceOf(
+        page.window.HTMLInputElement,
+      );
+    });
+  });
+
+  describe("attributes", () => {
+    it.each([
+      ["id", "test-id"],
+      ["aria-label", "test-aria-label"],
+    ])("applies %s", (attribute, expected) => {
+      const page = renderCheckbox({ [attribute]: expected });
+      expect(componentLocator(page).getAttribute(attribute)).toBe(expected);
+    });
+
+    it("applies classes", () => {
+      const page = renderCheckbox({ class: "test-class" });
+      expect(componentLocator(page).classList).toContain("test-class");
+      expect(componentLocator(page).classList).toContain("ds");
+      expect(componentLocator(page).classList).toContain("checkbox");
     });
 
     it("applies style", () => {
-      const { body } = renderCheckbox({ style: "color: red;" });
-      expect(body).toContain('style="color: red;"');
+      const page = renderCheckbox({ style: "color: orange;" });
+      expect(componentLocator(page).style.color).toBe("orange");
     });
   });
 
   describe("Checked state", () => {
     it("isn't checked by default", () => {
-      const { body } = renderCheckbox();
-      expect(body).not.toContain("checked");
+      const page = renderCheckbox({});
+      expect(componentLocator(page).checked).toBe(false);
     });
 
     it("can be checked", () => {
-      const { body } = renderCheckbox({ checked: true });
-      expect(body).toContain("checked");
+      const page = renderCheckbox({ checked: true });
+      expect(componentLocator(page).checked).toBe(true);
     });
   });
 
   describe("Group controlled", () => {
     it("isn't checked if group and value are undefined", () => {
-      const { body } = renderCheckbox({ group: undefined, value: undefined });
-      expect(body).not.toContain("checked");
+      const page = renderCheckbox({ group: undefined, value: undefined });
+      expect(componentLocator(page).checked).toBe(false);
     });
 
     it("isn't checked if group doesn't include value", () => {
-      const body = renderCheckbox({
+      const page = renderCheckbox({
         group: ["a", "b"],
         value: "c",
-      }).body;
-      expect(body).not.toContain("checked");
+      });
+      expect(componentLocator(page).checked).toBe(false);
     });
 
     it("is checked if group includes value", () => {
-      const { body } = renderCheckbox({
+      const page = renderCheckbox({
         group: ["a", "b", "c"],
         value: "c",
       });
-      expect(body).toContain("checked");
+      expect(componentLocator(page).checked).toBe(true);
     });
   });
 });
 
-function renderCheckbox(props?: CheckboxProps) {
+function renderCheckbox(props: CheckboxProps) {
   // @ts-expect-error TypeScript reports `Expression produces a union type that is too complex to represent.ts(2590)`
   return render(Component, { props });
+}
+
+function componentLocator(page: RenderResult): HTMLInputElement {
+  return page.getByRole("checkbox");
 }
