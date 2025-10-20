500ffd44af9d3a021c970d17ec4460ba438181a8
steciuk <a.t.steciuk@gmail.com>
2025-10-20 15:34:48 +0200

    test(IconText): Refactor tests

--- src/lib/components/IconText/IconText.ssr.test.ts
+++ src/lib/components/IconText/IconText.ssr.test.ts
@@ -1,28 +1,66 @@
-/* @canonical/generator-ds 0.10.0-experimental.2 */
+/* @canonical/generator-ds 0.10.0-experimental.5 */
 
+import { render } from "@canonical/svelte-ssr-test";
+import type { RenderResult } from "@canonical/svelte-ssr-test";
 import { createRawSnippet } from "svelte";
-import { render } from "svelte/server";
+import type { ComponentProps } from "svelte";
 import { describe, expect, it } from "vitest";
 import Component from "./IconText.svelte";
 
 describe("IconText SSR", () => {
-  const icon = createRawSnippet(() => ({
-    render: () => "<span>Icon</span>",
-  }));
-  const children = createRawSnippet(() => ({
-    render: () => "<span>Text</span>",
-  }));
+  const baseProps = {
+    icon: createRawSnippet(() => ({
+      render: () => "<span>Icon</span>",
+    })),
+    children: createRawSnippet(() => ({
+      render: () => "<span>Text</span>",
+    })),
+  } satisfies ComponentProps<typeof Component>;
 
-  it("doesn't throw and renders", () => {
-    expect(() => {
-      const { body } = render(Component, {
-        props: {
-          icon,
-          children,
-        },
+  describe("basics", () => {
+    it("doesn't throw", () => {
+      expect(() => {
+        render(Component, { props: { ...baseProps } });
+      }).not.toThrow();
+    });
+
+    it("renders", () => {
+      const page = render(Component, { props: { ...baseProps } });
+      expect(componentLocator(page)).toBeInstanceOf(page.window.HTMLElement);
+      expect(page.getByText("Icon")).toBeTruthy();
+      expect(page.getByText("Text")).toBeTruthy();
+    });
+  });
+
+  describe("attributes", () => {
+    it.each([
+      ["id", "test-id"],
+      ["aria-label", "test-aria-label"],
+    ])("applies %s", (attribute, expected) => {
+      const page = render(Component, {
+        props: { ...baseProps, [attribute]: expected },
       });
-      expect(body).toContain("<span>Icon</span>");
-      expect(body).toContain("<span>Text</span>");
-    }).not.toThrow();
+      expect(componentLocator(page).getAttribute(attribute)).toBe(expected);
+    });
+
+    it("applies classes", () => {
+      const page = render(Component, {
+        props: { ...baseProps, class: "test-class" },
+      });
+      expect(componentLocator(page).classList).toContain("test-class");
+      expect(componentLocator(page).classList).toContain("ds");
+      expect(componentLocator(page).classList).toContain("icon-text");
+    });
+
+    it("applies style", () => {
+      const page = render(Component, {
+        props: { ...baseProps, style: "color: orange;" },
+      });
+      expect(componentLocator(page).style.color).toBe("orange");
+    });
   });
 });
+
+function componentLocator(page: RenderResult): HTMLElement {
+  return page.getByTestId("icon-text");
+}
