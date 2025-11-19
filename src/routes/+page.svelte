d95e55d7e86bb25013ad1a3457495a668dc34b1a
steciuk <a.t.steciuk@gmail.com>
2025-11-19 17:56:51 +0100

    WIP

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
-  documentation
-</p>
+<script>
+  import Button from "$lib/components/Button/Button.svelte";
+  import { AddCanvas, IconProvider } from "$lib/components/icons/index.js";
+
+  const numberOfIcons = 7;
+  const shown = $state(Array.from({ length: numberOfIcons }, () => true));
+</script>
+
+<IconProvider>
+  {#each shown as isShown, i (i)}
+    <div style="margin-bottom: 16px;">
+      {#if isShown}
+        <Button onclick={() => (shown[i] = false)}>
+          <AddCanvas />
+        </Button>
+      {:else}
+        <Button onclick={() => (shown[i] = true)}>Show</Button>
+      {/if}
+    </div>
+  {/each}
+</IconProvider>
