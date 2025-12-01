8490727b7520f5eaef8e792011cc7942e27bbdf2
steciuk <a.t.steciuk@gmail.com>
2025-12-01 11:02:33 +0100

    feat: Add IconsOptimizationProvider

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,7 @@
     Comments,
     Desktop,
     Home,
+    IconsOptimizationProvider,
     LogOut,
     Search,
     User,
@@ -65,129 +66,131 @@
 
 <ThemeSetter {theme} />
 
-<GlobalShortcutsProvider>
-  <UseShortcuts shortcuts={[helpShortcut, toggleThemeShortcut]} />
-  <ShortcutsHelpSidePanel bind:this={modalMethods} />
+<IconsOptimizationProvider>
+  <GlobalShortcutsProvider>
+    <UseShortcuts shortcuts={[helpShortcut, toggleThemeShortcut]} />
+    <ShortcutsHelpSidePanel bind:this={modalMethods} />
 
-  <div class="app-layout">
-    <SideNavigation {expanded}>
-      {#snippet logo()}
-        <a href="/" aria-label="Launchpad Home" class="logo-link">
-          {#if expanded}
-            <div
-              aria-hidden="true"
-              transition:cssControlledFade={{
-                durationVar: "--transition-duration-side-navigation",
-                easingVar: "--transition-easing-side-navigation",
-              }}
-            >
-              <LaunchpadLogoText />
+    <div class="app-layout">
+      <SideNavigation {expanded}>
+        {#snippet logo()}
+          <a href="/" aria-label="Launchpad Home" class="logo-link">
+            {#if expanded}
+              <div
+                aria-hidden="true"
+                transition:cssControlledFade={{
+                  durationVar: "--transition-duration-side-navigation",
+                  easingVar: "--transition-easing-side-navigation",
+                }}
+              >
+                <LaunchpadLogoText />
+              </div>
+            {/if}
+            <div aria-hidden="true">
+              <LaunchpadLogo />
             </div>
-          {/if}
-          <div aria-hidden="true">
-            <LaunchpadLogo />
-          </div>
-        </a>
-      {/snippet}
-      {#snippet expandToggle(toggleProps)}
-        <form
-          method="POST"
-          action="/?/toggleSideNavigation"
-          style="display: contents;"
-          use:enhance={({ cancel }) => {
-            // If we have JS, abort the submission and don't bother the server
-            cancel();
-            expanded = !expanded;
-            // TODO: Use cookie parsing library. Kit uses: https://github.com/jshttp/cookie
-            document.cookie = `side-navigation-expanded=${expanded}; path=/; expires=${new Date(
-              Date.now() + 1000 * 60 * 60 * 24 * 365,
-            ).toUTCString()}`;
-          }}
-        >
-          <SideNavigation.ExpandToggle
-            {...toggleProps}
-            name="expanded"
-            value={String(!expanded)}
-          />
-        </form>
-      {/snippet}
-      <SideNavigation.LinkItem href="/">
-        Home
-        {#snippet icon()}
-          <Home />
+          </a>
         {/snippet}
-      </SideNavigation.LinkItem>
-      <!-- TODO: Placeholder links -->
-      <SideNavigation.LinkItem disabled>
-        View another MP
-        {#snippet icon()}
-          <Search />
-        {/snippet}
-      </SideNavigation.LinkItem>
-      <SideNavigation.LinkItem disabled>
-        Give feedback
-        {#snippet icon()}
-          <Comments />
-        {/snippet}
-      </SideNavigation.LinkItem>
-      {#snippet footer()}
-        <Popover position="inline-end span-block-start">
-          {#snippet trigger(triggerProps)}
-            <SideNavigation.ButtonItem {...triggerProps}>
-              {#snippet icon()}
-                <ColorPalette />
-              {/snippet}
-              Theme: {theme[0].toUpperCase() + theme.slice(1)}
-            </SideNavigation.ButtonItem>
-          {/snippet}
-          <ContextualMenuContent
-            style="margin-inline-start: var(--tmp-dimension-spacing-inline-xxs);"
+        {#snippet expandToggle(toggleProps)}
+          <form
+            method="POST"
+            action="/?/toggleSideNavigation"
+            style="display: contents;"
+            use:enhance={({ cancel }) => {
+              // If we have JS, abort the submission and don't bother the server
+              cancel();
+              expanded = !expanded;
+              // TODO: Use cookie parsing library. Kit uses: https://github.com/jshttp/cookie
+              document.cookie = `side-navigation-expanded=${expanded}; path=/; expires=${new Date(
+                Date.now() + 1000 * 60 * 60 * 24 * 365,
+              ).toUTCString()}`;
+            }}
           >
-            <form
-              method="POST"
-              action="/?/changeTheme"
-              use:enhance
-              style="display: contents;"
-            >
-              <ContextualMenuContent.Group style="min-width: 280px">
-                {#each themes as themeOption (themeOption)}
-                  {@const { Icon, label } = themesDisplay[themeOption]}
-                  <ContextualMenuContent.ButtonItem
-                    text={label}
-                    name="theme"
-                    value={themeOption}
-                    type="submit"
-                  >
-                    {#snippet icon()}
-                      <Icon />
-                    {/snippet}
-                  </ContextualMenuContent.ButtonItem>
-                {/each}
-              </ContextualMenuContent.Group>
-            </form>
-          </ContextualMenuContent>
-        </Popover>
+            <SideNavigation.ExpandToggle
+              {...toggleProps}
+              name="expanded"
+              value={String(!expanded)}
+            />
+          </form>
+        {/snippet}
+        <SideNavigation.LinkItem href="/">
+          Home
+          {#snippet icon()}
+            <Home />
+          {/snippet}
+        </SideNavigation.LinkItem>
         <!-- TODO: Placeholder links -->
         <SideNavigation.LinkItem disabled>
-          $username
+          View another MP
           {#snippet icon()}
-            <User />
+            <Search />
           {/snippet}
         </SideNavigation.LinkItem>
         <SideNavigation.LinkItem disabled>
-          Log out
+          Give feedback
           {#snippet icon()}
-            <LogOut />
+            <Comments />
           {/snippet}
         </SideNavigation.LinkItem>
-      {/snippet}
-    </SideNavigation>
+        {#snippet footer()}
+          <Popover position="inline-end span-block-start">
+            {#snippet trigger(triggerProps)}
+              <SideNavigation.ButtonItem {...triggerProps}>
+                {#snippet icon()}
+                  <ColorPalette />
+                {/snippet}
+                Theme: {theme[0].toUpperCase() + theme.slice(1)}
+              </SideNavigation.ButtonItem>
+            {/snippet}
+            <ContextualMenuContent
+              style="margin-inline-start: var(--tmp-dimension-spacing-inline-xxs);"
+            >
+              <form
+                method="POST"
+                action="/?/changeTheme"
+                use:enhance
+                style="display: contents;"
+              >
+                <ContextualMenuContent.Group style="min-width: 280px">
+                  {#each themes as themeOption (themeOption)}
+                    {@const { Icon, label } = themesDisplay[themeOption]}
+                    <ContextualMenuContent.ButtonItem
+                      text={label}
+                      name="theme"
+                      value={themeOption}
+                      type="submit"
+                    >
+                      {#snippet icon()}
+                        <Icon />
+                      {/snippet}
+                    </ContextualMenuContent.ButtonItem>
+                  {/each}
+                </ContextualMenuContent.Group>
+              </form>
+            </ContextualMenuContent>
+          </Popover>
+          <!-- TODO: Placeholder links -->
+          <SideNavigation.LinkItem disabled>
+            $username
+            {#snippet icon()}
+              <User />
+            {/snippet}
+          </SideNavigation.LinkItem>
+          <SideNavigation.LinkItem disabled>
+            Log out
+            {#snippet icon()}
+              <LogOut />
+            {/snippet}
+          </SideNavigation.LinkItem>
+        {/snippet}
+      </SideNavigation>
 
-    <main style:padding="1rem">
-      {@render children()}
-    </main>
-  </div>
-</GlobalShortcutsProvider>
+      <main style:padding="1rem">
+        {@render children()}
+      </main>
+    </div>
+  </GlobalShortcutsProvider>
+</IconsOptimizationProvider>
 
 <style>
   .app-layout {
