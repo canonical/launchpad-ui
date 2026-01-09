b788a851fc366bd6edc41fd60b628f0bdd04fc9a
steciuk <a.t.steciuk@gmail.com>
2026-01-09 12:04:31 +0100

    feat(job-details): Independent scrolls for log and details

--- src/routes/jobs/[id]/+page.svelte
+++ src/routes/jobs/[id]/+page.svelte
@@ -26,71 +26,69 @@
     />
   </div>
   <div class="details">
-    <div class="content-wrapper">
-      <div>
-        <h1>Job {data.job.title}</h1>
-        <p>{data.job.description}</p>
-        {#if data.job.status}
-          <!-- TODO: Job status chips -->
-          <JobStatus status={data.job.status} />
-        {/if}
-        <Chip
-          value={data.job.private ? "Private" : "Public"}
-          severity="information"
-          readonly
-        />
-      </div>
-      <section>
-        <h2 class="visually-hidden">Details</h2>
-        <DescriptionList>
-          <DescriptionList.Item name="ID">
-            {data.job.id}
-          </DescriptionList.Item>
-          <DescriptionList.Item name="Architecture">
-            {data.job.architecture}
-          </DescriptionList.Item>
-          <DescriptionList.Item name="Requested by">
-            {#if data.job.requested_by}
-              <UserChip userName={data.job.requested_by} />
-            {/if}
-          </DescriptionList.Item>
-          <DescriptionList.Item name="Created at">
-            {@render nullableDateTime(data.job.created_at)}
-          </DescriptionList.Item>
-          <DescriptionList.Item name="Updated at">
-            {@render nullableDateTime(data.job.updated_at)}
-          </DescriptionList.Item>
-          <DescriptionList.Item name="Started at">
-            {@render nullableDateTime(data.job.started_at)}
-          </DescriptionList.Item>
-          {#if data.job.completed_at}
-            <DescriptionList.Item name="Completed at">
-              {@render nullableDateTime(data.job.completed_at)}
-            </DescriptionList.Item>
-          {/if}
-        </DescriptionList>
-      </section>
-      <section>
-        <h2>Commands</h2>
-        <!-- TODO: Commands component -->
-        <ol>
-          {#each data.job.commands as command (command)}
-            <li>{command}</li>
-          {/each}
-        </ol>
-      </section>
-      <section>
-        <h2>Artifacts</h2>
-        <!-- TODO: Artifacts component -->
-        <ul>
-          {#each data.job.artifact_urls as artifact (artifact)}
-            <li style="word-break: break-word;">
-              {artifact}
-            </li>
-          {/each}
-        </ul>
-      </section>
+    <div>
+      <h1>Job {data.job.title}</h1>
+      <p>{data.job.description}</p>
+      {#if data.job.status}
+        <!-- TODO: Job status chips -->
+        <JobStatus status={data.job.status} />
+      {/if}
+      <Chip
+        value={data.job.private ? "Private" : "Public"}
+        severity="information"
+        readonly
+      />
     </div>
+    <section>
+      <h2 class="visually-hidden">Details</h2>
+      <DescriptionList>
+        <DescriptionList.Item name="ID">
+          {data.job.id}
+        </DescriptionList.Item>
+        <DescriptionList.Item name="Architecture">
+          {data.job.architecture}
+        </DescriptionList.Item>
+        <DescriptionList.Item name="Requested by">
+          {#if data.job.requested_by}
+            <UserChip userName={data.job.requested_by} />
+          {/if}
+        </DescriptionList.Item>
+        <DescriptionList.Item name="Created at">
+          {@render nullableDateTime(data.job.created_at)}
+        </DescriptionList.Item>
+        <DescriptionList.Item name="Updated at">
+          {@render nullableDateTime(data.job.updated_at)}
+        </DescriptionList.Item>
+        <DescriptionList.Item name="Started at">
+          {@render nullableDateTime(data.job.started_at)}
+        </DescriptionList.Item>
+        {#if data.job.completed_at}
+          <DescriptionList.Item name="Completed at">
+            {@render nullableDateTime(data.job.completed_at)}
+          </DescriptionList.Item>
+        {/if}
+      </DescriptionList>
+    </section>
+    <section>
+      <h2>Commands</h2>
+      <!-- TODO: Commands component -->
+      <ol>
+        {#each data.job.commands as command (command)}
+          <li>{command}</li>
+        {/each}
+      </ol>
+    </section>
+    <section>
+      <h2>Artifacts</h2>
+      <!-- TODO: Artifacts component -->
+      <ul>
+        {#each data.job.artifact_urls as artifact (artifact)}
+          <li style="word-break: break-word;">
+            {artifact}
+          </li>
+        {/each}
+      </ul>
+    </section>
   </div>
   <section class="log-container">
     <h2 class="visually-hidden">Log</h2>
@@ -126,28 +124,27 @@
 <style>
   .page {
     display: grid;
-    min-height: 100%;
-    grid-template-rows: auto 1fr;
-    grid-template-columns: minmax(300px, 1fr) 2fr;
+    height: 100vh;
+
+    grid-template:
+      "breadcrumbs log-container" auto
+      "details log-container" minmax(0, 1fr) / minmax(300px, 1fr) 2fr;
 
     --border-section-separator: var(--dimension-stroke-thickness-default) solid
       var(--tmp-color-border-low-contrast);
   }
 
   .breadcrumbs {
-    grid-column: 1;
+    grid-area: breadcrumbs;
     border-block-end: var(--border-section-separator);
   }
 
   .details {
-    grid-column: 1;
+    grid-area: details;
+    overflow: auto;
 
-    .content-wrapper {
-      position: sticky;
-      top: 0;
-      padding-block: var(--tmp-dimension-spacing-block-l);
-      padding-inline: var(--tmp-dimension-spacing-inline-l);
-    }
+    padding-block: var(--tmp-dimension-spacing-block-l);
+    padding-inline: var(--tmp-dimension-spacing-inline-l);
 
     h1 {
       font: var(--tmp-typography-h3);
@@ -170,8 +167,7 @@
   }
 
   .log-container {
-    grid-column: 2;
-    grid-row: 1 / -1;
+    grid-area: log-container;
     display: grid;
     grid-template-rows: subgrid;
 
@@ -179,8 +175,6 @@
       var(--tmp-color-border-default);
 
     .log-header {
-      position: sticky;
-      top: 0;
       background-color: var(--tmp-color-background-default);
       padding: var(--tmp-dimension-spacing-block-xs)
         var(--tmp-dimension-spacing-inline-l);
@@ -192,8 +186,6 @@
     }
 
     .trailing-bar {
-      position: sticky;
-      bottom: 0;
       display: flex;
       align-items: center;
       gap: var(--tmp-dimension-spacing-inline-xxs);
@@ -205,16 +197,28 @@
 
   @media (max-width: 620px) {
     .page {
-      grid-template-columns: auto;
+      height: auto;
+      grid-template:
+        "breadcrumbs" auto
+        "details" auto
+        "log-container" auto / 1fr;
     }
 
     .log-container {
-      grid-column: 1;
-      grid-row: auto;
+      display: block;
+
       border-inline-start: none;
       border-block-start: var(--border-section-separator);
 
-      display: block;
+      .log-header {
+        position: sticky;
+        top: 0;
+      }
+
+      .trailing-bar {
+        position: sticky;
+        bottom: 0;
+      }
     }
   }
 </style>
