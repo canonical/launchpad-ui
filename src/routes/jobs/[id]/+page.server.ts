7230421c3ef9ad60887148b9d0bdda47ebf9d6ad
steciuk <a.t.steciuk@gmail.com>
2026-01-19 17:33:34 +0100

    feat(job details): Display the log

--- src/routes/jobs/[id]/+page.server.ts
+++ src/routes/jobs/[id]/+page.server.ts
@@ -1,8 +1,9 @@
 import { error } from "@sveltejs/kit";
 import { jobManager } from "$lib/api/job-manager/client.js";
+import { stripAnsi } from "$lib/utils/index.js";
 import type { PageServerLoad } from "./$types";
 
-export const load = (async ({ params }) => {
+export const load = (async ({ params, fetch }) => {
   const id = parseInt(params.id, 10);
   if (isNaN(id)) {
     error(400, "Invalid job ID");
@@ -14,6 +15,7 @@ export const load = (async ({ params }) => {
         job_id: id,
       },
     },
+    fetch,
   });
 
   if (response.error) {
@@ -21,5 +23,55 @@ export const load = (async ({ params }) => {
     error(500, "Failed to fetch job details");
   }
 
-  return { job: response.data };
+  const job = response.data;
+
+  const defaultLogUrl = job.log_urls?.find((url) =>
+    // @ts-expect-error until log_urls is typed properly
+    url.endsWith("default.log"),
+  );
+
+  if (!defaultLogUrl) {
+    return {
+      job,
+      log: [],
+    };
+  }
+
+  // @ts-expect-error until log_urls is typed properly
+  const logResponse = await fetch(defaultLogUrl);
+
+  if (!logResponse.ok) {
+    console.error(
+      `Failed to fetch default log: ${logResponse.status} ${logResponse.statusText}`,
+    );
+    return { job, log: [] };
+  }
+
+  const logText = await logResponse.text();
+  const log = logText.split("\n").map((line) => parseLogLine(line));
+
+  return { job, log };
 }) satisfies PageServerLoad;
+
+function parseLogLine(rawLine: string) {
+  const line = stripAnsi(rawLine);
+
+  // 2025-12-05 21:34:29.446 [admin] mkdir -p /home/ubuntu/.ssh
+  // 2025-12-05 21:34:29.454 Starting proxy setup...
+  const match = line.match(
+    /^(\d{4}-\d{2}-\d{2}) (\d{2}:\d{2}:\d{2}\.\d{3}) (.*)$/,
+  );
+
+  if (match) {
+    const [, date, time, message] = match;
+    return {
+      timestamp: `${date}T${time}Z`,
+      message,
+    };
+  }
+
+  return {
+    timestamp: new Date(0).toISOString(),
+    message: line,
+  };
+}
