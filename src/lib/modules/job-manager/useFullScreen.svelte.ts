07333fe8bf12c78b0a8935f57778196aa32adfa3
steciuk <a.t.steciuk@gmail.com>
2026-01-23 13:05:53 +0100

    feat(useFullScreen): Add js-docs

--- src/lib/modules/job-manager/useFullScreen.svelte.ts
+++ src/lib/modules/job-manager/useFullScreen.svelte.ts
@@ -5,7 +5,7 @@ const queryParamName = "full-screen";
 
 export function useFullScreen() {
   const isEnabled = $derived(
-    page.url.searchParams.get(queryParamName) !== null,
+    page.url.searchParams.get(queryParamName) === "true",
   );
 
   const toggleHref = $derived.by(() => {
@@ -20,6 +20,9 @@ export function useFullScreen() {
     return url.pathname + url.search;
   });
 
+  /**
+   * Enable full-screen mode by adding the query parameter to the URL.
+   */
   const enable = () => {
     // Non-reactive URL is ok, as page itself is reactive
     // eslint-disable-next-line svelte/prefer-svelte-reactivity
@@ -29,6 +32,9 @@ export function useFullScreen() {
     goto(url.pathname + url.search);
   };
 
+  /**
+   * Disable full-screen mode by removing the query parameter from the URL.
+   */
   const disable = () => {
     // Non-reactive URL is ok, as page itself is reactive
     // eslint-disable-next-line svelte/prefer-svelte-reactivity
@@ -38,12 +44,25 @@ export function useFullScreen() {
     goto(url.pathname + url.search);
   };
 
+  /**
+   * Toggle full-screen mode by adding or removing the query parameter from the URL.
+   */
   const toggle = () => (isEnabled ? disable() : enable());
 
   return {
+    /**
+     * Whether full-screen mode is enabled.
+     */
     get isEnabled() {
       return isEnabled;
     },
+    /**
+     * Get href to toggle full-screen mode.
+     *
+     * @example <a href={toggleHref}>Toggle Full Screen</a>
+     *
+     * This link will add or remove the query parameter to enable or disable full-screen mode.
+     */
     get toggleHref() {
       return toggleHref;
     },
