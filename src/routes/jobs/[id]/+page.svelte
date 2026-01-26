6d28fff170c237706caeb519b4808fdf89419478
steciuk <a.t.steciuk@gmail.com>
2026-01-26 12:36:57 +0100

    chore(job-details): Extract JobDetails to a separate component

--- src/routes/jobs/[id]/+page.svelte
+++ src/routes/jobs/[id]/+page.svelte
@@ -1,28 +1,14 @@
 <script lang="ts">
-  import { DownloadIcon, FileIcon } from "@canonical/svelte-icons";
   import { MediaQuery } from "svelte/reactivity";
-  import { ButtonPrimitive } from "$lib/components/common/index.js";
+  import { Breadcrumbs, Log } from "$lib/components/index.js";
+  import type { TimeZone } from "$lib/components/index.js";
   import {
-    Breadcrumbs,
-    Chip,
-    DateTime,
-    DescriptionList,
-    Link,
-    Log,
-    UserChip,
-  } from "$lib/components/index.js";
-  import type { DateTimeProps, TimeZone } from "$lib/components/index.js";
-  import {
-    CommandList,
-    JobStatusIcon,
-  } from "$lib/launchpad-components/index.js";
-  import {
+    JobDetails,
     LogHeader,
     NoLogs,
     TrailingBar,
     useFullScreen,
   } from "$lib/modules/job-manager/index.js";
-  import { bytesToHumanReadable } from "$lib/utils/index.js";
   import type { PageProps } from "./$types";
   import { browser } from "$app/environment";
   import { resolve } from "$app/paths";
@@ -67,118 +53,7 @@
       ]}
       style="grid-area: breadcrumbs; border-block-end: var(--border-section-separator); align-self: stretch;"
     />
-    <div class="details">
-      <div>
-        <h1>{job.title || job.id}</h1>
-        <p>{job.description}</p>
-        <div class="chips">
-          {#if job.status}
-            <Chip
-              value={job.status}
-              readonly
-              severity={job.status === "FAILED"
-                ? "negative"
-                : job.status === "FINISHED"
-                  ? "positive"
-                  : "neutral"}
-            >
-              {#snippet icon()}
-                <JobStatusIcon status={job.status} aria-hidden="true" />
-              {/snippet}
-            </Chip>
-          {/if}
-          <Chip
-            value={job.private ? "Private" : "Public"}
-            severity="information"
-            readonly
-          />
-        </div>
-      </div>
-      <section>
-        <h2 class="visually-hidden">Details</h2>
-        <DescriptionList>
-          <DescriptionList.Item name="ID">
-            {job.id}
-          </DescriptionList.Item>
-          <DescriptionList.Item name="Architecture">
-            {job.architecture}
-          </DescriptionList.Item>
-          <DescriptionList.Item name="Requested by">
-            {#if job.requested_by}
-              <UserChip userName={job.requested_by} />
-            {/if}
-          </DescriptionList.Item>
-          <DescriptionList.Item name="Created at">
-            {@render nullableDateTime(job.created_at)}
-          </DescriptionList.Item>
-          <DescriptionList.Item name="Updated at">
-            {@render nullableDateTime(job.updated_at)}
-          </DescriptionList.Item>
-          <DescriptionList.Item name="Started at">
-            {@render nullableDateTime(job.started_at)}
-          </DescriptionList.Item>
-          <DescriptionList.Item name="Completed at">
-            {@render nullableDateTime(job.completed_at)}
-          </DescriptionList.Item>
-        </DescriptionList>
-      </section>
-      <section>
-        <h2 class="section-header">Commands</h2>
-        <CommandList>
-          {#each job.commands as command, i (i)}
-            <!-- 
-            TODO(job-manager):
-              - pass command status
-              - command not as `unknown` but proper type
-              - link command to log entry
-            
-            TODO: Syntax highlighting for command
-          -->
-            <CommandList.Command
-              status={null}
-              command={command as string}
-              href={undefined}
-            />
-          {/each}
-        </CommandList>
-      </section>
-      {#if job.artifacts?.length}
-        <section>
-          <div
-            class="section-header"
-            style="display: flex; justify-content: space-between; align-items: center;"
-          >
-            <h2>Artifacts</h2>
-            <!--
-          TODO:
-            - Add href, when zip endpoint is available
-            - Replace with a proper Button component
-          -->
-            <ButtonPrimitive
-              as="a"
-              disabled
-              style="display: flex; gap: var(--tmp-dimension-spacing-inline-xs); align-items: center; padding: var(--tmp-dimension-spacing-block-minimum) var(--tmp-dimension-spacing-inline-xs);"
-            >
-              <DownloadIcon />Download All
-            </ButtonPrimitive>
-          </div>
-          <ul class="artifacts">
-            {#each job.artifacts as { url, size } (url)}
-              <li>
-                <Link href={url} download>
-                  <span class="icon-aligner">
-                    <FileIcon aria-hidden="true" />
-                  </span>
-                  <span class="artifact-name">
-                    {url.split("/").at(-1)} ({bytesToHumanReadable(size)})
-                  </span>
-                </Link>
-              </li>
-            {/each}
-          </ul>
-        </section>
-      {/if}
-    </div>
+    <JobDetails {job} />
   {/if}
   <section class="log-container" class:log-full-screen={fullScreen.isEnabled}>
     <h2 class="visually-hidden" id={logHeaderId}>Log</h2>
@@ -229,12 +104,6 @@
   </section>
 </div>
 
-{#snippet nullableDateTime(date: DateTimeProps["date"] | null | undefined)}
-  {#if date}
-    <DateTime {date} absolute />
-  {/if}
-{/snippet}
-
 <style>
   .page {
     display: grid;
@@ -254,66 +123,6 @@
       grid-template:
         "log-container" var(--header-height)
         "log-container" minmax(0, 1fr) / 1fr;
-    }
-  }
-
-  .details {
-    grid-area: details;
-    overflow: auto;
-
-    padding-block: var(--tmp-dimension-spacing-block-l);
-    padding-inline: var(--tmp-dimension-spacing-inline-l);
-
-    h1 {
-      font: var(--tmp-typography-h3);
-    }
-
-    h2 {
-      font: var(--tmp-typography-h5);
-    }
-
-    .chips {
-      display: flex;
-      gap: var(--tmp-dimension-spacing-inline-xxs);
-      margin-block-start: var(--tmp-dimension-spacing-block-xs);
-    }
-
-    section:first-of-type {
-      margin-block-start: var(--tmp-dimension-spacing-block-l);
-    }
-
-    section + section {
-      margin-block-start: var(--tmp-dimension-spacing-block-m);
-      padding-block-start: var(--tmp-dimension-spacing-block-m);
-      border-block-start: var(--border-section-separator);
-    }
-
-    .section-header {
-      margin-block-end: var(--tmp-dimension-spacing-block-m);
-    }
-
-    .artifacts {
-      list-style: none;
-      display: grid;
-      gap: var(--tmp-dimension-spacing-block-xs);
-
-      :global(a) {
-        display: flex;
-        align-items: start;
-        gap: var(--tmp-dimension-spacing-inline-xs);
-        color: var(--tmp-color-text-default);
-
-        > .icon-aligner {
-          height: 1lh;
-          flex-shrink: 0;
-          display: grid;
-          place-items: center;
-        }
-
-        > .artifact-name {
-          word-break: break-word;
-        }
-      }
     }
   }
 
